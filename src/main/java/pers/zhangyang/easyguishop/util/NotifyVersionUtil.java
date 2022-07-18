e0d33230241338a1d3f94676030621cc003b6fd5
ZhangYang <3319527246@qq.com>
2022-07-19 03:42:07 +0800

    修复BUG： - 修复了某些情况下（可能是安装了聊天插件的情况）输入消息发送出去的情况

--- src/main/java/pers/zhangyang/easyguishop/util/NotifyVersionUtil.java
+++ src/main/java/pers/zhangyang/easyguishop/util/NotifyVersionUtil.java
@@ -1,6 +1,7 @@
 package pers.zhangyang.easyguishop.util;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.scheduler.BukkitRunnable;
 import pers.zhangyang.easyguishop.EasyGuiShop;
 import pers.zhangyang.easyguishop.yaml.MessageYaml;
 
@@ -12,25 +13,32 @@ public class NotifyVersionUtil {
 
 
     public static void notifyVersion(CommandSender sender) {
-        String latestVersion;
-        try {
-            latestVersion = ResourceUtil.readFirstLine(new URL("https://zhangyang0204.github.io/easy-gui-shop/index.html"));
-        } catch (Throwable e) {
-            latestVersion = null;
-        }
-        List<String> list;
-        if (latestVersion != null) {
-            list = MessageYaml.INSTANCE.getStringList("message.chat.notifyVersion");
-            HashMap<String, String> hashMap = new HashMap<>();
-            hashMap.put("{current_version}", EasyGuiShop.instance.getDescription().getVersion());
-            hashMap.put("{latest_version}", latestVersion);
-            if (list != null) {
-                ReplaceUtil.replace(list, hashMap);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                String latestVersion;
+                try {
+                    latestVersion = ResourceUtil.readFirstLine(new URL("https://zhangyang0204.github.io/easy-gui-shop/index.html"));
+                } catch (Throwable e) {
+                    latestVersion = null;
+                }
+                List<String> list;
+                if (latestVersion != null) {
+                    list = MessageYaml.INSTANCE.getStringList("message.chat.notifyVersion");
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("{current_version}", EasyGuiShop.instance.getDescription().getVersion());
+                    hashMap.put("{latest_version}", latestVersion);
+                    if (list != null) {
+                        ReplaceUtil.replace(list, hashMap);
+                    }
+                } else {
+                    list = MessageYaml.INSTANCE.getStringList("message.chat.failureGetLatestVersion");
+                }
+                MessageUtil.sendMessageTo(sender, list);
             }
-        } else {
-            list = MessageYaml.INSTANCE.getStringList("message.chat.failureGetLatestVersion");
-        }
-        MessageUtil.sendMessageTo(sender, list);
+        }.runTaskAsynchronously(EasyGuiShop.instance);
+
     }
 
 }
